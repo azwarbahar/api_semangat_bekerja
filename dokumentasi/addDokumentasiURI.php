a59f2b86d09720d6c42f75e2fba14e19a115177e
azwarbahar <azwarbahar07@gmail.com>
2022-10-21 16:18:24 +0800

    new upload image

--- dokumentasi/addDokumentasiURI.php
+++ dokumentasi/addDokumentasiURI.php
@@ -5,15 +5,15 @@ error_reporting(E_ERROR | E_PARSE);
 
 // $foto = $_POST['foto'];
 
+$jenis_dokumentasi = $_POST['jenis_dokumentasi'];
+$urutan = $_POST['urutan'];
 $outlet_id = $_POST['outlet_id'];
+$kunjungan_id = $_POST['kunjungan_id'];
 $sales_id = $_POST['sales_id'];
-$status_checkin = $_POST['status_checkin'];
-$jam = $_POST['jam'];
+// $nama_foto = "-";
 $tanggal = $_POST['tanggal'];
-$latitude = $_POST['latitude'];
-$longitude = $_POST['longitude'];
-$masuk_area = $_POST['masuk_area'];
-$kendala = $_POST['kendala'];
+$jam = $_POST['jam'];
+
 
 // SET FOTO
 $foto = $_FILES['foto']['name'];
@@ -21,20 +21,21 @@ $ext = pathinfo($foto, PATHINFO_EXTENSION);
 $nama_foto = "image_" . time() . "." . $ext;
 $file_tmp = $_FILES['foto']['tmp_name'];
 
-$query = "INSERT INTO tb_kunjungan values(null,'$outlet_id',
+
+$query = "INSERT INTO tb_dokumentasi values(null, '$jenis_dokumentasi',
+                                        '$urutan',
+                                        '$outlet_id',
+                                        '$kunjungan_id',
                                         '$sales_id',
-                                        '$status_checkin',
-                                        '$jam',
-                                        '$tanggal',
-                                        '$latitude',
-                                        '$longitude',
-                                        '$masuk_area',
                                         '$nama_foto',
+                                        '$tanggal',
+                                        '$jam',
                                         null,
                                         null)";
 
 if (mysqli_query($conn, $query)) {
-    move_uploaded_file($file_tmp, '../upload/kunjungan/' . $nama_foto);
+
+    move_uploaded_file($file_tmp, '../upload/dokumentasi/' . $nama_foto);
 
     $result["kode"] = "1";
     $result["pesan"] = "Success!";
